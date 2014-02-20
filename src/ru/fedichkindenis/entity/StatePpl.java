0ec90eb793ad0e53c12fd9799c691ae2c6dcfdcb
Fedichkin Denis <fedichkin.dy@gmail.com>
2014-02-20 23:40:12 +0400

    entity StatePpl

---
+++ src/ru/fedichkindenis/entity/StatePpl.java
@@ -0,0 +1,106 @@
+package ru.fedichkindenis.entity;
+
+import ru.fedichkindenis.enums.StatusPpl;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Fedichkin.DY
+ * Date: 20.02.14
+ * Time: 23:12
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Table(name = "state_ppl")
+public class StatePpl {
+
+    /**
+     * Идентификатор состояния колониста
+     */
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    /**
+     * Ссылка на игру
+     */
+    @ManyToOne
+    @JoinColumn(name = "game", nullable = false)
+    private Game game;
+
+    /**
+     * Количество дней проведеных в капсуле
+     */
+    @Column(name = "day_capsule", nullable = false)
+    private Integer daysCapsule;
+
+    /**
+     * Количество дней проведеных в капсуле
+     */
+    @Enumerated(EnumType.ORDINAL)
+    @Column(name = "stat", nullable = false)
+    private StatusPpl stat;
+
+    /**
+     * Дата прилета на Луну
+     */
+    @Column(name = "add_date", nullable = false)
+    @Temporal(value = TemporalType.DATE)
+    private Date addDate;
+
+    /**
+     * Дата смерти или выбытия
+     */
+    @Column(name = "del_date", nullable = true)
+    @Temporal(value = TemporalType.DATE)
+    private Date delDate;
+
+    /**********GETTERS AND SETTERS**********/
+
+    public Long getId() {
+        return id;
+    }
+
+    public Game getGame() {
+        return game;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+
+    public Integer getDaysCapsule() {
+        return daysCapsule;
+    }
+
+    public void setDaysCapsule(Integer daysCapsule) {
+        this.daysCapsule = daysCapsule;
+    }
+
+    public StatusPpl getStat() {
+        return stat;
+    }
+
+    public void setStat(StatusPpl stat) {
+        this.stat = stat;
+    }
+
+    public Date getAddDate() {
+        return addDate;
+    }
+
+    public void setAddDate(Date addDate) {
+        this.addDate = addDate;
+    }
+
+    public Date getDelDate() {
+        return delDate;
+    }
+
+    public void setDelDate(Date delDate) {
+        this.delDate = delDate;
+    }
+}
---
+++ src/ru/fedichkindenis/enums/StatusPpl.java
@@ -0,0 +1,23 @@
+package ru.fedichkindenis.enums;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Fedichkin.DY
+ * Date: 20.02.14
+ * Time: 23:28
+ * To change this template use File | Settings | File Templates.
+ */
+public enum StatusPpl {
+
+    IN_GAME(0, "В игре"),
+    DIE(1, "Умер"),
+    OUT_GAME(2, "Покинул луну");
+
+    private Integer id;
+    private String name;
+
+    private StatusPpl(Integer id, String name){
+        this.id = id;
+        this.name = name;
+    }
+}
