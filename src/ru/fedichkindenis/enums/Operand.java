bb49a9787954411e6cfa47481f95ae29fb992f16
Fedichkin Denis <fedichkin.dy@gmail.com>
2014-03-07 00:07:32 +0400

    HQL operand finished

--- src/ru/fedichkindenis/enums/Operand.java
+++ src/ru/fedichkindenis/enums/Operand.java
@@ -5,68 +5,62 @@ package ru.fedichkindenis.enums;
  * User: Fedichkin.DY
  * Date: 27.02.14
  * Time: 23:01
- * To change this template use File | Settings | File Templates.
+ * Множество операндов и запросы для получения их значений
  */
 public enum Operand {
 
     START_COST_EARTH(
             0,
             "стартовая цена Земли",
-            "select startCost from InitMarketEarth ime where ime.resources = :resources and ime.game = :game"),
+            "operand.start_cost_earth"),
 
     CONST_VALUE_EARTH(
             1,
             "константа количества Земли",
-            "select constValue from InitMarketEarth ime where ime.resources = :resources and ime.game = :game"),
+            "operand.const_value_earth"),
 
     START_VALUE_EARTH(
             2,
             "стартовое количество на Земле",
-            "select startValue from InitMarketEarth ime where ime.resources = :resources and ime.game = :game"),
+            "operand.start_value_earth"),
 
     CONST_CONSUM_EARTH(
             3,
             "константа потребления Земли",
-            "select constConsum from InitMarketEarth ime where ime.resources = :resources and ime.game = :game"),
+            "operand.start_consum_earth"),
 
     MULTI_PRICE_INCR_EARTH(
             4,
             "мультипликатор роста цен Земли",
-            "select multiPriceIncr from InitMarketEarth ime where ime.resources = :resources and ime.game = :game"),
+            "operand.multi_price_incr_earth"),
 
     CURR_SALE_COST_EARTH(
             5,
             "текущая цена продажи на Земле",
-            "select saleCost MarketEarth me where me.resources = :resources and me.game = :game and me.gameDate = :gameDate"),
+            "operand.curr_sale_cost_earth"),
 
     CURR_BUY_COST_EARTH(
             5,
             "текущая цена покупки на Земле",
-            "select buyCost from MarketEarth me where me.resources = :resources and me.game = :game and me.gameDate = :gameDate"),
+            "operand.curr_buy_cost_earth"),
 
     CURR_VALUE_EARTH(
             6,
             "текущее количество на Земле",
-            "select val from MarketEarth me where me.resources = :resources and me.game = :game and me.gameDate = :gameDate"),
+            "operand.curr_value_earth"),
 
-    /*SUM_ALL_CREDIT(
+    SUM_ALL_CREDIT(
             7,
             "общее количество кредитов в игре",
-            "select count from GameResStat grs join grsResourcesStatistics",
-            "count",
-            ""),
+            "operand.sum_all_credit_player"),
     COUNT_PLAYER(
             8,
             "общее количество игроков",
-            "UsrGame",
-            "count(user)",
-            ""),
+            "operand.count_player"),
 
     COUNT_PPL(9,
             "общее количество колонистов",
-            "Ppl",
-            "count(id)",
-            "");*/
+            "operand.count_ppl");
 
     private Integer id;
     private String name;
